7926887e0d20e81e6e7544cadac2bc112059d6db
ch3rk4 <pety.shurupov@gmail.com>
2025-06-22 19:53:57 +0300

    create models

--- users/models.py
+++ users/models.py
@@ -1,3 +1,20 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, verbose_name='Email')
+    phone = models.CharField(max_length=20, blank=True, null=True, verbose_name='Телефон')
+    city = models.CharField(max_length=100, blank=True, null=True, verbose_name='Город')
+    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True, verbose_name='Аватар')
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return self.email

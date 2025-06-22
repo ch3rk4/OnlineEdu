1df891b5e2895cd658eb0a471add8e836bf54f5e
ch3rk4 <pety.shurupov@gmail.com>
2025-06-22 20:08:11 +0300

    create admins

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Персональная информация', {'fields': ('first_name', 'last_name', 'phone', 'city', 'avatar')}),
+        ('Права доступа', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Важные даты', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
+    list_display = ('email', 'first_name', 'last_name', 'phone', 'city', 'is_staff')
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'city')
+    search_fields = ('email', 'first_name', 'last_name')
+    ordering = ('email',)
